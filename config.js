029cbe9ef0f77518e50c53ef3a3ce7c9d8cf9101
Sarada <saradawrites@gmail.com>
2026-08-24 10:38:21 +0530

    Update API_URL in configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PORTFOLIO_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbw94Ys5MZBV5RuUC65GW0OkwAO2l0NBhK-eIRUl_q4s-olbMYBhMNxKCZla1XPkL0ZV/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyhjl-nMnhgNVhRvCYH8uFt8gewlQW7BfX2MUib5dtWc3B-v8QYVQX4Il_6WxPlLnph/exec',
   APP_NAME: 'My Finance',
   CURRENCY: 'INR',
   REQUEST_TIMEOUT_MS: 60000,
