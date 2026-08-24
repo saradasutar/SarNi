c5b0111979a2d48b508bece25b80ddd59b31bb19
Sarada <saradawrites@gmail.com>
2026-08-24 23:15:29 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PORTFOLIO_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyCJfHHYUkv6VrMlcKWMoC_i1cryMbow9JAVm3lfVvgqiYDBMdEHcfiTpyLWEI0TTmd/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbx0Se_UHDk1zWdPHcKm3WRBFEVHl2CalsaPRqVa020qrpj0Crq-l7T1W_5I8ciGhF1w/exec',
   APP_NAME: 'My Finance',
   CURRENCY: 'INR',
   REQUEST_TIMEOUT_MS: 60000,
